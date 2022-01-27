c740574c422bb1bec73d3cbc10f5a70b93e45211
Hari-15 <91254397+Hari-15@users.noreply.github.com>
2022-01-27 15:57:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function Submit() {
+    player1_name = document.getElementById('input1').value;
+    player2_name = document.getElementById('input2').value;
+
+    localStorage.setItem("Player 1", player1_name);
+    localStorage.setItem("Player 2", player2_name);
+    
+    window.location = "game_page.html";
+}
